64ef0e89199f66a3bb9f49c06376200f4055e9bf
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-02-05 12:48:21 +0530

    Update outputs.tf

--- dev/outputs.tf
+++ dev/outputs.tf
@@ -1,11 +1,11 @@
 output "resource_group_name" {
-  value = "dem-resource-group1"
+  value = "dem-resource-group1-dev"
 }
 
 output "storage_account_name" {
-  value = "dem-storage-acc1"
+  value = "dem-storage-acc1-dev"
 }
 
 output "container_name" {
-  value = "dem-container1"
+  value = "dem-container1-dev"
 }
