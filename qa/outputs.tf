2aaa13187eefc50eab0074536a87d8794410fbfe
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-02-05 12:50:11 +0530

    Create outputs.tf

---
+++ qa/outputs.tf
@@ -0,0 +1,11 @@
+output "resource_group_name" {
+  value = "dem-resource-group1-qa"
+}
+
+output "storage_account_name" {
+  value = "dem-storage-acc1-qa"
+}
+
+output "container_name" {
+  value = "dem-container1-qa"
+}
