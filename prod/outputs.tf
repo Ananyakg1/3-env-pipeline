17434b493e2b7a50dde0598b5012b587f242d22e
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-02-05 12:51:43 +0530

    Create outputs.tf

---
+++ prod/outputs.tf
@@ -0,0 +1,11 @@
+output "resource_group_name" {
+  value = "dem-resource-group1-p"
+}
+
+output "storage_account_name" {
+  value = "dem-storage-acc1-p"
+}
+
+output "container_name" {
+  value = "dem-container1-p"
+}
