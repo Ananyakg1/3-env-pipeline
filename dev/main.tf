17334b4fbee57b2b9c35e273f4d05638879c460e
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-02-06 10:31:06 +0530

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -4,10 +4,10 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
-  location = var.location
+  location = "East US"
 }
 
-resource "azurerm_storage_account" "storage" {
+resource "azurerm_storage_account" "sa" {
   name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
@@ -17,6 +17,7 @@ resource "azurerm_storage_account" "storage" {
 
 resource "azurerm_storage_container" "container" {
   name                  = var.container_name
-  storage_account_name  = azurerm_storage_account.storage.name
+  storage_account_name  = azurerm_storage_account.sa.name
   container_access_type = "private"
 }
+
